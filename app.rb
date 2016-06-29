8ed574f403bafa94e7da3a5e7dd20208fbb5b471
yyangflatiron <yichen.yang@flatironschool.com>
2016-06-29 16:16:09 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/' do
+    erb :display_puppy
+  end
 end
